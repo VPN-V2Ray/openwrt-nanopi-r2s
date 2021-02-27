d4472283dcd4abedbd3cc65294194d8014ec6f1d
VPN-V2Ray <46552880+VPN-V2Ray@users.noreply.github.com>
2021-02-27 10:06:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,8 +15,8 @@ rm -rf openwrt
 git clone -b openwrt-21.02 https://github.com/openwrt/openwrt.git openwrt
 
 # patch openwrt
-cd "$proj_dir/openwrt"
-cat "$proj_dir/patches"/*.patch | patch -p1
+#cd "$proj_dir/openwrt"
+#cat "$proj_dir/patches"/*.patch | patch -p1
 
 # obtain feed list
 cd "$proj_dir/openwrt"
@@ -51,26 +51,26 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks custom/
 svn co https://github.com/fw876/helloworld/trunk/naiveproxy custom/naiveproxy
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 custom/redsocks2
 # luci-app-openclash
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash custom/luci-app-openclash
+#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash custom/luci-app-openclash
 # luci-app-filebrowser
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/ctcgfw/luci-app-filebrowser custom/luci-app-filebrowser
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/ctcgfw/filebrowser custom/filebrowser
 # luci-app-arpbind
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind custom/luci-app-arpbind
 # luci-app-xlnetacc
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/lean/luci-app-xlnetacc custom/luci-app-xlnetacc
+#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/lean/luci-app-xlnetacc custom/luci-app-xlnetacc
 # luci-app-oled
-git clone --depth 1 https://github.com/NateLol/luci-app-oled.git custom/luci-app-oled
+#git clone --depth 1 https://github.com/NateLol/luci-app-oled.git custom/luci-app-oled
 # luci-app-unblockmusic
-svn co https://github.com/cnsilvan/luci-app-unblockneteasemusic/trunk/luci-app-unblockneteasemusic custom/luci-app-unblockneteasemusic
-svn co https://github.com/cnsilvan/luci-app-unblockneteasemusic/trunk/UnblockNeteaseMusic custom/UnblockNeteaseMusic
+#svn co https://github.com/cnsilvan/luci-app-unblockneteasemusic/trunk/luci-app-unblockneteasemusic custom/luci-app-unblockneteasemusic
+#svn co https://github.com/cnsilvan/luci-app-unblockneteasemusic/trunk/UnblockNeteaseMusic custom/UnblockNeteaseMusic
 # luci-app-autoreboot
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot custom/luci-app-autoreboot
 # luci-app-vsftpd
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/lean/luci-app-vsftpd custom/luci-app-vsftpd
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/lean/vsftpd-alt custom/vsftpd-alt
+#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/lean/luci-app-vsftpd custom/luci-app-vsftpd
+#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/lean/vsftpd-alt custom/vsftpd-alt
 # luci-app-netdata
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-netdata custom/luci-app-netdata
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-netdata custom/luci-app-netdata
 # ddns-scripts
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/lean/ddns-scripts_aliyun custom/ddns-scripts_aliyun
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/lean/ddns-scripts_dnspod custom/ddns-scripts_dnspod
